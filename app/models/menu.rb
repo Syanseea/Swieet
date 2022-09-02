b91bd339aa2ea62c7ece7d5dea4581c8decb9a9b
Syanseea <uemana0518@gmail.com>
2022-09-02 12:06:12 +0000

    update

--- app/models/menu.rb
+++ app/models/menu.rb
@@ -3,7 +3,7 @@ class Menu < ApplicationRecord
   belongs_to :user
   has_many :themes, dependent: :destroy
   accepts_nested_attributes_for :themes, allow_destroy: true
-
+#存在したらOk
   validates :title, presence: true
   validates :dating, presence: true
 
